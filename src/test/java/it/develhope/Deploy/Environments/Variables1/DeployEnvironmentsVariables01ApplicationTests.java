54e8d09c23864fb26cdfe8d5d9828b325af5c0cd
Emilio <emi00@gmail.com>
2022-11-10 10:20:01 +0100

    first commit

---
+++ src/test/java/it/develhope/Deploy/Environments/Variables1/DeployEnvironmentsVariables01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.Deploy.Environments.Variables1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEnvironmentsVariables01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
